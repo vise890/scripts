d6d7d9932a160f41889f0ccdee09cd5a58e12a98
Martino Visintin <martino.visintin@ovoenergy.com>
2017-02-13 13:12:35 +0000

    add noddy display script

---
+++ 4k-only.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "==> switch off others"
+xrandr \
+--output VIRTUAL1 --off  \
+--output eDP1 --off \
+--output HDMI1 --off \
+--output HDMI2 --off \
+--output DP2 --off
+
+echo "==> setup monitor on DP1 (4k screen)"
+xrandr \
+--output DP1 --mode 3840x2160 \
+--pos 0x0 --rotate normal
+
+echo "==> set dpi"
+xrandr \
+  --dpi 184
+
+echo "==> load xres"
+xrdb ~/.Xresources
+
